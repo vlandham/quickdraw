18c1317b9d800e257ba0a7c25ff49ec277895b6b
Jim Vallandingham <vlandham@gmail.com>
2017-06-18 14:52:29 -0700

    update with more data

--- src/hist.js
+++ src/hist.js
@@ -3,30 +3,27 @@ import * as d3 from 'd3';
 
 export default function createHist() {
   const margin = { top: 20, right: 20, bottom: 20, left: 20 };
-  const width = 900;
-  const height = 400;
+  let width = 900;
+  let height = 400;
   let g = null;
+  let bar = null;
   let data = [];
+  let overlap = true;
+  let xDomain = null;
 
-  const xScale = d3.scaleLinear()
-    .rangeRound([0, width])
-    .domain([0, 25]);
+  const xScale = d3.scaleLinear();
 
-  const yScale = d3.scaleLinear()
-    .range([height, 0]);
+  const yScale = d3.scaleLinear();
 
   const chart = function wrapper(selection, rawData) {
     data = rawData;
-
-    const svg = d3.select(selection).append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom);
-
-    g = svg.append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-
     setupScales();
-    update();
+
+    if (overlap) {
+      setupSvg(selection);
+    }
+
+    update(selection);
   };
 
   function setupScales() {
@@ -39,21 +36,71 @@ export default function createHist() {
       xMaxs.push(d3.max(data[id].hist, d => d.x1));
       yMins.push(d3.min(data[id].hist, d => d.freq));
       yMaxs.push(d3.max(data[id].hist, d => d.freq));
+      // data[id].hist.forEach(h => h.key = id);
     });
-    // const xExtent = d3.extent(data.dog.hist, d => d.x1);
-    // xScale.domain([0, d3.max(xMaxs)]);
-    xScale.domain([0, 25]);
 
-    // const freqExtent = d3.extent(data.dog.hist, d => d.freq);
-    yScale.domain([0, d3.max(yMaxs)]);
+    // xScale.domain([0, d3.max(xMaxs)]);
+
+    if (!xDomain) {
+      xDomain = [0, d3.max(xMaxs)];
+    }
+
+    xScale
+      .rangeRound([0, width])
+      .domain(xDomain);
+      // .domain([0, d3.max(xMaxs)]);
+
+    yScale
+      .range([height, 0])
+      .domain([0, d3.max(yMaxs)]);
   }
 
-  function update() {
+  function setupSvg(selection) {
+    const svg = d3.select(selection).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom);
+
+    g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    g.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(xScale));
+  }
+
+  function update(selection) {
+    let barData = [];
     data.keys.forEach((id) => {
+      barData = barData.concat(data[id].hist);
+    });
+
+    // draw background bars
+    const x0s = d3.set(barData, d => d.x0);
+
+    g.selectAll('.background')
+      .data(x0s.values())
+      .enter()
+      .append('rect')
+      .attr('class', 'background')
+      .attr('fill', 'white')
+      .attr('x', d => xScale(+d))
+      .attr('y', 0)
+      .attr('width', d => xScale(+d + 1) - xScale(+d))
+      .attr('height', height)
+      .on('mouseover', mouseover)
+      .on('mouseout', mouseout);
+
+    // loop through all keys
+    data.keys.forEach((id) => {
+      if (!overlap) {
+        setupSvg(selection);
+      }
+
       const idG = g.append('g')
         .classed(id, true);
-      const bar = idG.selectAll('.bar')
-        .data(data[id].hist)
+      bar = idG.selectAll('.bar')
+        .data(data[id].hist, d => id + d.x0)
         .enter().append('g')
         .classed('bar', true)
         .classed(id, true)
@@ -62,10 +109,46 @@ export default function createHist() {
       bar
         .append('rect')
         .attr('x', 1)
+        .attr('opacity', 0.6)
         .attr('width', d => xScale(d.x1) - xScale(d.x0))
-        .attr('height', d => height - yScale(d.freq));
+        .attr('height', d => height - yScale(d.freq))
+        .attr('pointer-events', 'none');
     });
   }
+
+  function mouseover(d) {
+    d3.selectAll('.bar').filter(b => b.x0 === +d)
+      .classed('active', true);
+  }
+
+  function mouseout() {
+    d3.selectAll('.bar')
+      .classed('active', false);
+  }
+
+  chart.width = function setWidth(value) {
+    if (!arguments.length) { return width; }
+    width = value;
+    return this;
+  };
+
+  chart.overlap = function setOverlap(value) {
+    if (!arguments.length) { return overlap; }
+    overlap = value;
+    return this;
+  };
+
+  chart.height = function setHeight(value) {
+    if (!arguments.length) { return height; }
+    height = value;
+    return this;
+  };
+
+  chart.xDomain = function setX(value) {
+    if (!arguments.length) { return xDomain; }
+    xDomain = value;
+    return this;
+  };
 
   return chart;
 }
--- src/main.js
+++ src/main.js
@@ -7,19 +7,24 @@ import '../index.html';
 import './style';
 
 const draw = createDraw();
-const limitDraw = createDraw().limit(14);
+// const limitDraw = createDraw().limit(14);
 const hist = createHist();
 
-function display(error, dogs, cats, data) {
+function display(error, dogs, cats, dogCat, birds) {
   console.log(error);
-  draw('#dogs', dogs);
-  limitDraw('#dogs-title', dogs);
-  limitDraw('#cats', cats);
-  hist('#hist', data);
+  draw.limit(null)('#dogs', dogs);
+  draw.limit(14)('#dogs-title', dogs);
+  draw.limit(14)('#cats', cats);
+  hist.xDomain([0, 24])('#hist', dogCat);
+  hist
+    .xDomain([0, 18])
+    .width(200)
+    .height(200).overlap(false)('#birds', birds);
 }
 
 d3.queue()
   .defer(d3.json, 'data/dogs.json')
   .defer(d3.json, 'data/cats.json')
   .defer(d3.json, 'data/dog_cat_out.json')
+  .defer(d3.json, 'data/circle_bird_swan_flamingo_out.json')
   .await(display);
