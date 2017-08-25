90e6e49ef56518450263820d7e60989c26540b32
Jim Vallandingham <vlandham@gmail.com>
2017-08-25 14:28:29 -0700

    add category colors

--- src/swarm.js
+++ src/swarm.js
@@ -9,8 +9,22 @@ export default function createSwarm() {
   let g = null;
   let svg = null;
   let data = [];
+  // const types = d3.set();
   const radius = 12;
   const scale = radius / 255;
+
+  let showTypes = true;
+
+  const typeColors = {
+    animal: '#FFA726',
+    food: '#EF5350',
+    nature: '#9CCC65',
+    location: '#26A69A',
+    vehicle: '#29B6F6',
+    body_part: '#A1887F',
+    abstract: '#7E57C2',
+    object: '#BDBDBD',
+  };
 
   const line = d3.line()
     .x(d => d[0])
@@ -36,6 +50,11 @@ export default function createSwarm() {
     g = svg.append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
+    g.append('g')
+      .attr('class', 'legend')
+      .attr('pointer-events', 'none')
+      .attr('transform', `translate(${width - 100},${0})`);
+
     update();
   };
 
@@ -45,6 +64,8 @@ export default function createSwarm() {
       // d.icon = d.drawings[0];
       d.drawings = d.drawings.map(d => ({ drawing: d }))
       d.iconIndex = 0;
+
+      // types.add(d.type);
     });
     return rdata;
   }
@@ -66,6 +87,29 @@ export default function createSwarm() {
       .orientation('horizontal')
       .side('symetric')
       .arrange();
+  }
+
+  function createLegend() {
+
+    const legend = g.select('.legend')
+      .selectAll('.key')
+      .data(Object.keys(typeColors))
+
+    const legendE = legend.enter()
+      .append('g')
+      .attr('class', 'key')
+      .attr('transform', (d, i) => `translate(${-margin.top},${((radius + 5) * i)})`);
+
+    legendE.append('circle')
+      .attr('r', radius / 2)
+      .attr('fill', d => colorFor(d))
+      .attr('opacity', 0.8)
+
+    legendE.append('text')
+      .attr('x', radius + 5)
+      .attr('dy', 5)
+      .text(d => d.replace('_', ' '));
+
   }
 
   function update() {
@@ -111,13 +155,34 @@ export default function createSwarm() {
       .transition()
       .duration(500)
       .attr('transform', d => `translate(${d.x}, ${(height / 2) + d.y})`);
+
+    updateFill();
+  }
+
+  function colorFor(type) {
+    if (!showTypes) {
+      return 'white';
+    }
+
+    return typeColors[type];
+  }
+
+  function updateFill() {
+    g.selectAll('.doodle').selectAll('.background')
+      .transition()
+      .duration(500)
+      .attr('fill', d => colorFor(d.datum.type))
+
+    if (showTypes) {
+      createLegend();
+    }
   }
 
   function mouseover(d) {
     const gr = d3.select(this);
     gr.raise();
 
-    const expandR = radius * 2;
+    const expandR = radius * 3;
 
     gr.select('.background')
       .transition()
@@ -129,7 +194,7 @@ export default function createSwarm() {
     gr.insert('circle', '.background')
       .attr('opacity', 0.8)
       .attr('class', 'temp-background')
-      .attr('fill', 'white')
+      .attr('fill', colorFor(d.datum.type))
       .attr('pointer-events', 'none')
       .attr('r', expandR * 2)
       .attr('stroke-width', 0);
@@ -203,6 +268,13 @@ export default function createSwarm() {
     if (!arguments.length) { return positionKey; }
     positionKey = value;
     update();
+    return this;
+  };
+
+  chart.colorBy = function setColorBy(value) {
+    if (!arguments.length) { return showTypes; }
+    showTypes = value;
+    updateFill();
     return this;
   };
 
